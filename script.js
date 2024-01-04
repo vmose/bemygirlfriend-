dfb27fcbb9f02bca1522fe8150189be03517904e
vmose <vicmose.vm@gmail.com>
2024-01-04 23:32:41 +0300

    Add audio

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function moveButton() {
     noButton.style.left = `${x}px`;
     noButton.style.top = `${y}px`;
 }
+
+// script.js
+
+let audioPlayer = new Audio('https://www.youtube.com/embed/CwGbMYLjIpQ?autoplay=1&loop=1&mute=1');
+let isAudioPlaying = true;
+
+function toggleAudio() {
+  if (isAudioPlaying) {
+    audioPlayer.pause();
+  } else {
+    audioPlayer.play();
+  }
+  isAudioPlaying = !isAudioPlaying;
+}
+
